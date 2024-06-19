47b45f5d389b8b834b701af648360c27e564321b
arelokesh-7 <arelokesh7@gmail.com>
2024-06-19 17:06:18 +0530

    files upload

---
+++ tasks/flask/easy/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# TODO: Implement the task requested in the issue description
+@app.route('/')
+def hello():
+    return "Hello, Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
